a841c85582ba81854e171fb290c6c62bc623797e
noaGolWix <noagol@wix.com>
2024-03-27 16:49:55 +0200

    adding cors and port as env var

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // Importing express module
 const express = require('express')
+const cors = require('cors')
 
 // Getting a server instance from express
 const app = express()
+
+// Handeling cors
+app.use(cors())
 
 // Declering get function
 app.get('/', (req, res) => {
@@ -10,6 +14,6 @@ app.get('/', (req, res) => {
 })
 
 // Starting server on trafic from port
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('server is running on port 3000')
 })
